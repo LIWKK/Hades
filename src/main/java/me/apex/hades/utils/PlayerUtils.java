9357dd26fb08b731c5fadd8cba9761ea5addfda0
LIWKK <35636315+LIWKK@users.noreply.github.com>
2020-05-28 03:05:02 +0300

    Added isNearGround

--- src/main/java/me/apex/hades/utils/PlayerUtils.java
+++ src/main/java/me/apex/hades/utils/PlayerUtils.java
@@ -29,6 +29,20 @@ public class PlayerUtils {
         return false;
     }
 
+    //Credits to jonhan
+    public static boolean isNearGround(Location location) {
+        double expand = 0.3;
+        for (double x = -expand; x <= expand; x += expand) {
+            for (double z = -expand; z <= expand; z += expand) {
+                if (location.clone().add(x, -0.5001, z).getBlock().getType() != Material.AIR) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
     public static boolean blockNearHead(Player player) {
         double expand = 0.3;
         for (double x = -expand; x <= expand; x += expand) {
