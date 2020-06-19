534c6728a103f678e368a313f48215d2d4ed06bb
undersquire <apexdev23@gmail.com>
2020-06-19 10:13:17 -0700

    Syntax fix

--- src/main/java/me/apex/hades/check/impl/movement/invalid/InvalidB.java
+++ src/main/java/me/apex/hades/check/impl/movement/invalid/InvalidB.java
@@ -14,17 +14,18 @@ public class InvalidB extends Check {
 
     @Override
     public void onHandle(PacketEvent e, User user) {
-        if(e instanceof FlyingEvent) {
-            if(user.getDeltaY() > 0.0) {
+        if (e instanceof FlyingEvent) {
+            if (user.getDeltaY() > 0.0) {
                 ticks++;
-            }else if(user.getDeltaY() < 0.0) {
-                if(ticks > 0 && ticks < 5 && !user.isOnGround() && elapsed(user.getTick(), user.getUnderBlockTick()) > 40 && elapsed(user.getTick(), user.getVelocityTick()) < 100) {
+            } else if (user.getDeltaY() < 0.0) {
+                if (ticks > 0 && ticks < 5 && !user.isOnGround() && elapsed(user.getTick(), user.getUnderBlockTick()) > 40 && elapsed(user.getTick(), user.getVelocityTick()) < 100) {
                     flag(user, "too few air ticks, t: " + ticks);
                 }
                 ticks = 0;
-            if (user.getLocation().getWorld().getDifficulty() == Difficulty.PEACEFUL) return;
-            if (user.isSprinting() && user.getPlayer().getFoodLevel() < 6){
-                flag(user, "sprinting while hunger levels are low.");
+                if (user.getLocation().getWorld().getDifficulty() == Difficulty.PEACEFUL) return;
+                if (user.isSprinting() && user.getPlayer().getFoodLevel() < 6) {
+                    flag(user, "sprinting while hunger levels are low.");
+                }
             }
         }
     }
