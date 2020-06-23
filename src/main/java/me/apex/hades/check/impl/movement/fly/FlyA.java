2bc383a278386484b4bf1c07efa27b5b14801226
undersquire <apexdev23@gmail.com>
2020-06-23 09:39:56 -0700

    Fixed Fly Issues

--- src/main/java/me/apex/hades/check/impl/movement/fly/FlyA.java
+++ src/main/java/me/apex/hades/check/impl/movement/fly/FlyA.java
@@ -6,7 +6,6 @@ import me.apex.hades.check.CheckInfo;
 import me.apex.hades.event.impl.packetevents.FlyingEvent;
 import me.apex.hades.user.User;
 import me.apex.hades.util.PlayerUtil;
-import me.apex.hades.util.TaskUtil;
 
 @CheckInfo(name = "Fly", type = "A")
 public class FlyA extends Check {
@@ -19,17 +18,13 @@ public class FlyA extends Check {
                     && user.getPlayer().getVelocity().getY() < -0.075
                     && elapsed(user.getTick(), user.getLiquidTick()) > 20
                     && !user.isInWeb()
-                    && !user.isOnClimbableBlock()
+                    && elapsed(user.getTick(), user.getClimbableTick()) < 20
                     && !PlayerUtil.isOnClimbable(user.getPlayer())
                     && elapsed(user.getTick(), user.getFlyingTick()) > 40
                     && user.getPlayer().getVehicle() == null
                     && user.getTick() > 5
                     && elapsed(user.getTick(), user.getVelocityTick()) > 100) {
-                TaskUtil.task(() -> {
-                    if (!PlayerUtil.isClimbableBlock(user.getLocation().subtract(0, 1, 0).getBlock())) {
-                        flag(user, "y motion higher than 0, m: " + user.getDeltaY());
-                    }
-                });
+                flag(user, "y motion higher than 0, m: " + user.getDeltaY());
             }
         }
     }
