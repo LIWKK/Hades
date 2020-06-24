662a31fa5ca9fe4d7b02f392fefe4bea542753f8
undersquire <apexdev23@gmail.com>
2020-06-24 11:28:52 -0700

    Update AutoClickerC.java

--- src/main/java/me/apex/hades/check/impl/combat/autoclicker/AutoClickerC.java
+++ src/main/java/me/apex/hades/check/impl/combat/autoclicker/AutoClickerC.java
@@ -4,25 +4,34 @@ import io.github.retrooper.packetevents.event.PacketEvent;
 import me.apex.hades.HadesPlugin;
 import me.apex.hades.check.Check;
 import me.apex.hades.check.CheckInfo;
+import me.apex.hades.event.impl.packetevents.FlyingEvent;
 import me.apex.hades.event.impl.packetevents.SwingEvent;
 import me.apex.hades.user.User;
 
 @CheckInfo(name = "AutoClicker", type = "C")
 public class AutoClickerC extends Check {
 
-    private long start;
+    private int ticks;
+    private double clicksPerSecond;
 
     @Override
     public void onHandle(PacketEvent e, User user) {
-        if (e instanceof SwingEvent) {
-            if (user.isDigging() || user.isRightClickingBlock()) return;
-            if (time() - start >= 1000L) {
-                if (user.getCPS() > HadesPlugin.getInstance().getConfig().getInt("Max-CPS")) {
-                    flag(user, "CPS: " + user.getCPS());
-                }
-                user.setCPS(0);
-                start = time();
-            } else user.setCPS(user.getCPS() + 1);
+        if(e instanceof SwingEvent) {
+            if(user.isDigging() || ticks > 10) {
+                ticks = 0;
+                return;
+            }
+
+            double speed = 1000 / ((ticks * 50.0) > 0 ? (ticks * 50.0) : 1);
+            clicksPerSecond = ((clicksPerSecond * 19) + speed) / 20;
+
+            if(clicksPerSecond > HadesPlugin.getInstance().getConfig().getInt("Max-CPS")) {
+                flag(user, "CPS: " + user.getCPS());
+            }
+
+            ticks = 0;
+        }else if(e instanceof FlyingEvent) {
+            ticks++;
         }
     }
 }
